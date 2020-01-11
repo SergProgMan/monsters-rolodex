7b2255d21c46d5ae579edb57b8559cbc5f81bfb7
sergey <sergey.semibratyev@wp-rock.com>
2020-01-11 22:13:39 +0200

    created search component and made some styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
 class App extends Component {
-
   constructor() {
     super();
+
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
   }
 
   componentDidMount(){
@@ -20,11 +26,21 @@ class App extends Component {
   }
 
   render () {
-      return(
-        <div className="App">
-          <CardList monsters={this.state.monsters}/>
-        </div>
-      )    
+    const { monsters, searchField } = this.state; 
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return(
+      <div className="App">
+        <h1> Monstrs Rolodex </h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    )    
   }
 }
 
