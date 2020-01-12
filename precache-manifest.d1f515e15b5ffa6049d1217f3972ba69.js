d36a34028954fd9a3c2e1b0a26eb6c7786d6e04b
SergProgMan <semibratyev@gmail.com>
2020-01-12 11:57:53 +0200

    Updates

---
+++ precache-manifest.d1f515e15b5ffa6049d1217f3972ba69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887b5a4ed82e78e7e34195116474bc6b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ff185c4a502456248ad0",
+    "url": "/monsters-rolodex/static/css/main.4f54de5c.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff185c4a502456248ad0",
+    "url": "/monsters-rolodex/static/js/main.01329684.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
